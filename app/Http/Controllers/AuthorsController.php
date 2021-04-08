965dce93acdf043daba9bf5bad2a00d59d5abc28
Alexandra Waurig <alexandra@elisana.digital>
2021-04-08 16:37:37 +0200

    author validation tests

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -29,9 +29,7 @@ class AuthorsController extends Controller
 
     public function store()
     {
-        Author::create(request()->only([
-            'name', 'dob',
-        ]));
+        Author::create($this->validateRequest());
     }
 
     /**
@@ -77,5 +75,13 @@ class AuthorsController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    protected function validateRequest()
+    {
+        return request()->validate([
+            'name' => 'required',
+            'dob' => 'required',
+        ]);
     }
 }
--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -14,15 +14,34 @@ class AuthorManagementTest extends TestCase
     
     public function test_an_author_can_be_created()
     {
-        $this->post('/authors', [
-            'name' => 'Author Name',
-            'dob' => '05/14/1988',
-        ]);
+        $this->post('/authors', $this->data());
 
         $author =  Author::all();
 
         $this->assertCount(1, $author);
         $this->assertInstanceOf(Carbon::class, $author->first()->dob);
         $this->assertEquals('1988/14/05', $author->first()->dob->format('Y/d/m'));
+    }
+
+    public function test_a_name_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['name' => '']));
+
+        $response->assertSessionHasErrors('name');
+    }
+
+    public function test_a_dob_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['dob' => '']));
+
+        $response->assertSessionHasErrors('dob');
+    }
+
+    private function data()
+    {
+        return [
+            'name' => 'Author Name',
+            'dob' => '05/14/1988',
+        ];
     }
 }
